85b0880016ba0a6494ebbfda959cf303db0e1db1
Dunkode <edersonvidal137@gmail.com>
2022-10-27 21:35:30 -0300

    Implementação do gerenciamentos de faces reconhecidas

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
-import cv2 as cv
+from os import system
+from time import sleep
 
-cv.imread()
+from src.recognizedFacesService import RecongnizedFacesService
+
+while True:
+    system('cls')
+
+    recognizedFacesServ = RecongnizedFacesService()
+
+    print("{:-^40}".format("SecurityAid"))
+
+    print('''
+[1] Cadastrar rosto
+[2] Iniciar monitoramento
+[0] Fechar aplicativo
+    ''')
+    
+    try:
+        choice = int(input(">> "))
+
+        match choice:
+            case 0:
+                quit()
+            case 1:
+                pass
+            case 2:
+                print(recognizedFacesServ.getAuthorizedFaces())
+                pass
+            
+        sleep(3)
+        
+    except ValueError:
+        print("Digite uma opção válida!")
---
+++ src/recognizedFacesService.py
@@ -0,0 +1,43 @@
+from os import path, getcwd, mkdir
+from os.path import join, exists
+from glob import glob
+
+EXTENSION_FILES = ["png", "jpeg", "jpg"]
+
+class RecongnizedFacesService():
+
+    def __init__(self):
+        self.current_dir = getcwd()
+        self.registred_faces_dir = join(self.current_dir + "\\data\\registred_faces")
+        self.createRecognizedFacesDir()
+        self.__list_of_files = []
+        self.__list_of_names = []
+        self.__number_of_faces = 0
+
+    def getNamesFromFaces(self):
+        return self.__list_of_names
+
+    def getNumberOfFaces(self):
+        return self.__number_of_faces
+
+    def getAuthorizedFaces(self):
+        return self.__list_of_files
+
+    def loadAutorizedFaces(self):
+        for ext in EXTENSION_FILES:
+            self.__list_of_files = [ f for f in glob( path + f"*.{ext}") ]
+        
+        self.loadNameFromFaceFiles()
+
+    def loadNameFromFaceFiles(self):
+        for file in self.__list_of_files:
+            name = ""
+            for ext in EXTENSION_FILES:
+                name = file.replace(self.registred_faces_dir, "").replace(f".{ext}")
+            
+            self.__list_of_names.append(name)
+
+    def createRecognizedFacesDir(self):
+        if not exists(self.registred_faces_dir):
+            # mkdir(self.current_dir + "\\data")
+            mkdir(self.registred_faces_dir)
